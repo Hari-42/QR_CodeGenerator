fb9ce39a64234b5edf1fb67b8b3f7df59d385c4c
Hari-42 <harisaranmohanathas@hotmail.com>
2024-12-07 01:41:22 +0100

    created a window with tkinter

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import tkinter as tk
 import qrcode
 
-_input = input("Enter a link or a text:")
-img = qrcode.make(_input)
+# create Tkinter window
+root = tk.Tk()
+root.title("QR Code Generator")
+root.geometry("400x400")
+
+root.mainloop()
+
+
+# _input = input("Enter a link or a text:")
+#img = qrcode.make(_input)
 
 
 # Save the image
-img.save('example_qrcode.png')
+#img.save('example_qrcode.png')
