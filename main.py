2286a86c51704be322ac2a9646f25cc9ddce344f
Hari-42 <harisaranmohanathas@hotmail.com>
2024-12-08 00:48:07 +0100

    created a label and input field

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ root = tk.Tk()
 root.title("QR Code Generator")
 root.geometry("400x400")
 
+# create label and input field
+label_input = tk.Label(root, text="Enter Link or Text:", font=("Arial", 12))
+label_input.pack(pady=10)
+
+input_entry = tk.Entry(root, width=40, font=("Arial", 12))
+input_entry.pack(pady=5)
 root.mainloop()
 
 
