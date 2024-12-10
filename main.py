82a3fab37e66d5138c38e81e93eb796847c7014a
Hari-42 <harisaranmohanathas@hotmail.com>
2024-12-10 10:44:49 +0100

    created "Generate QR Code" button

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ root.geometry("400x400")
 label_input = tk.Label(root, text="Enter Link or Text:", font=("Arial", 12))
 label_input.pack(pady=10)
 
+# create a input entry field
 input_entry = tk.Entry(root, width=40, font=("Arial", 12))
 input_entry.pack(pady=5)
+
+# Add a button to generate the QR code
+generate_button = tk.Button(root, text="Generate QR Code", font=("Arial", 12), bg="lightgray", fg="black")
+generate_button.pack(pady=15)
 root.mainloop()
 
 
